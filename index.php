8ba99e04652dd7b18ce39b83fa3731753b26f076
Grace Pai <gpai@uci.edu>
2016-05-19 01:04:10 -0700

    removed analtyics script

--- index.php
+++ index.php
@@ -6,24 +6,6 @@
 		<script src="task.js"></script>
 		<link type="text/css" href="style.css" rel="stylesheet">
 		<title>Realtime Map | SFPD Incidents </title>
-		<!-- start google analytics scripts -->
-		<script type="text/javascript">
-			(function(i, s, o, g, r, a, m) {
-				i['GoogleAnalyticsObject'] = r;
-				i[r] = i[r] ||
-				function() {
-					(i[r].q = i[r].q || []).push(arguments)
-				}, i[r].l = 1 * new Date();
-				a = s.createElement(o), m = s.getElementsByTagName(o)[0];
-				a.async = 1;
-				a.src = g;
-				m.parentNode.insertBefore(a, m)
-			})(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
-
-			ga('create', 'UA-48438325-1', 'gracepai.com');
-			ga('send', 'pageview');
-		</script>
-		<!-- end google analytics scripts -->
 	</head>
 
 	<body>
